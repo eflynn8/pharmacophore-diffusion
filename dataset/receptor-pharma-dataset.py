d4066bc0c26b62bbafa2579318f14edfd72c3483
Emma Flynn <elf828@gmail.com>
2024-02-23 00:55:07 -0500

    Adding save and load tensors, build_complex_graph changes

--- dataset/receptor-pharma-dataset.py
+++ dataset/receptor-pharma-dataset.py
@@ -7,12 +7,13 @@ import dgl
 from dgl.dataloading import GraphDataLoader
 import torch
 import numpy as np
+from torch_cluster import radius_graph
 
-from data_processing.pdbbind_processing import (build_initial_complex_graph,
-                                                get_pocket_atoms, parse_ligand,
-                                                parse_protein, get_ot_loss_weights)
+# from data_processing.pdbbind_processing import (build_initial_complex_graph,
+#                                                 get_pocket_atoms, parse_ligand,
+#                                                 parse_protein, get_ot_loss_weights)
 
-class ProteinLigandDataset(dgl.data.DGLDataset):
+class ProteinPharmacophoreDataset(dgl.data.DGLDataset):
 
     def __init__(self, name: str, 
         processed_data_file: str,
@@ -27,10 +28,16 @@ class ProteinLigandDataset(dgl.data.DGLDataset):
         self.graph_cutoffs = graph_cutoffs
         self.prot_elements = prot_elements
 
+        # if subsample_pharms is True, getitem will return random resamplings of pharmacophores to augment dataset
+        self.subsample_pharms = subsample_pharms
+
         # If load_data is true, tensor will be constructed from data pkl files
         self.load_data = load_data
         self.data_files = data_files
         self.rec_file = rec_file
+
+        # define filepath of data
+        self.processed_data_file: Path = Path(processed_data_file)
 
         if self.load_data:
             if not self.data_files or not self.rec_file:
@@ -40,76 +47,86 @@ class ProteinLigandDataset(dgl.data.DGLDataset):
                     data = pickle.load(f)
                 
                 ## Collect data entries into lists
-                rec_file_name = [prot[0] for prot in data]
-                lig_file_name = [ph[1] for ph in data]
-                lig_obj = [ph[2] for ph in data]
-                lig_pos_arr = [lig[3][0] for lig in data]
-                lig_feat_arr = [lig[3][1] for lig in data]
-                rec_pos_arr = [rec[4][0] for rec in data]
-                rec_feat_arr = [rec[4][1] for rec in data]
+                prot_file_name = [prot[0] for prot in data]
+                pharm_file_name = [ph[1] for ph in data]
+                pharm_obj = [ph[2] for ph in data]
+                pharm_pos_arr = [pharm[3][0] for pharm in data]
+                pharm_feat_arr = [pharm[3][1] for pharm in data]
+                prot_pos_arr = [prot[4][0] for prot in data]
+                prot_feat_arr = [prot[4][1] for prot in data]
 
-                ## Convert lig_feat_arr and lig_pos_arr into single list
+                ## Convert pharm_feat_arr and pharm_pos_arr into single list
                 ## And encode pharmacophore feats as one-hot
-                lig_idx = []
-                lig_pos = []
-                lig_feat = []
+                pharm_idx = []
+                pharm_pos = []
+                pharm_feat = []
                 idx = 0
                 
-                for i, entry in enumerate(lig_pos_arr):
-                    lig_pos.extend(entry)
-                    lig_idx.append(idx)
+                for i, entry in enumerate(pharm_pos_arr):
+                    pharm_pos.extend(entry)
+                    pharm_idx.append(idx)
                     idx += len(entry) - 1
-                    lig_idx.append(idx)
+                    pharm_idx.append(idx)
                     idx += 1
 
-                    lig_feat_arr.extend(one_hot_encode_pharms(lig_feat[i]))
+                    pharm_feat.extend(one_hot_encode_pharms(pharm_feat_arr[i]))
                 
                 ## Create dictionary to encode protein elements
                 ele_idx_map = { element: idx for idx, element in enumerate(self.prot_elements) }
 
-                ## Convert rec_feat_arr and rec_pos_arr into single list
+                ## Convert prot_feat_arr and prot_pos_arr into single list
                 ## And encode protein elements as one-hot 
-                prot_idxs = []
+                prot_idx = []
                 prot_pos = []
                 prot_feat = []
                 idx = 0
-                for i, entry in enumerate(rec_pos_arr):
+                for i, entry in enumerate(prot_pos_arr):
                     prot_pos.extend(entry)
-                    prot_idxs.append(idx)
+                    prot_idx.append(idx)
                     idx += len(entry) - 1
-                    prot_idxs.append(idx)
+                    prot_idx.append(idx)
                     idx += 1
 
-                    prot_feat.extend(one_hot_encode_prots(rec_feat_arr[i], ele_idx_map, len(self.prot_elements)))
+                    prot_feat.extend(one_hot_encode_prots(prot_feat_arr[i], ele_idx_map, len(self.prot_elements)))
 
                 ## Make data into tensor
-                
+                self.prot_file_name = torch.tensor(prot_file_name)
+                self.pharm_file_name = torch.tensor(pharm_file_name)
+                self.pharm_obj = torch.tensor(pharm_obj)
+                self.pharm_pos = torch.tensor(pharm_pos)
+                self.pharm_feat = torch.tensor(pharm_feat)
+                self.prot_pos = torch.tensor(prot_pos)
+                self.prot_feat = torch.tensor(prot_feat)
+                self.pharm_idx = torch.tensor(pharm_idx)
+                self.prot_idx = torch.tensor(prot_idx)
 
-                # ## Set up dictionary of relevant data
-                # data_dict = {}
-                # data_dict['prot_file_name'] = rec_file_name
-                # data_dict['pharm_file_name'] = lig_file_name
-                # data_dict['pharm_obj'] = lig_obj
-                # data_dict['pharm_pos'] = lig_pos
-                # data_dict['pharm_feat'] = lig_feat
-                # data_dict['prot_pos'] = rec_pos
-                # data_dict['prot_feat'] = rec_feat
-
-        # define filepath of data
-        self.processed_data_file: Path = Path(processed_data_file)
-
-        # if subsample_pharms is True, getitem will return random resamplings of pharmacophores to augment dataset
-        self.subsample_pharms = subsample_pharms
-
+                ## Save list of tensors to processed_data_file
+                torch.save([self.prot_file_name, self.pharm_file_name, self.pharm_obj, self.pharm_pos, self.pharm_feat,
+                            self.prot_pos, self.prot_feat, self.pharm_idx, self.prot_idx], processed_data_file)
+        else:
+            data = torch.load(processed_data_file)
+            self.prot_file_name = data[0]
+            self.pharm_file_name = data[1]
+            self.pharm_obj = data[2]
+            self.pharm_pos = data[3]
+            self.pharm_feat = data[4]
+            self.prot_pos = data[5]
+            self.prot_feat = data[6]
+            self.pharm_idx = data[7]
+            self.prot_idx = data[8]
 
         super().__init__(name=name) # this has to happen last because this will call self.process()
 
     def __getitem__(self, i):
+        
+        pharm_start_idx, pharm_end_idx = self.pharm_idx[i:i+2]
+        prot_start_idx, prot_end_idx = self.prot_idx[i:i+2]
 
-        pharm_pos = self.pharm_pos[i]
-        pharm_feat = self.pharm_feat[i]
-        prot_pos = self.prot_pos[i]
-        prot_feat = self.prot_feat[i]
+        pharm_pos = self.pharm_pos[pharm_start_idx:pharm_end_idx]
+        pharm_feat = self.pharm_feat[pharm_start_idx:pharm_end_idx]
+        prot_pos = self.prot_pos[prot_start_idx:prot_end_idx]
+        prot_feat = self.prot_feat[prot_start_idx:prot_end_idx]
+        # rec_res_idx = self.rec_res_idx[rec_start_idx:rec_end_idx]
 
         ## Subsample pharmacophore features if subsample_pharms is True
         # select random indices, grab for both pos and feats, build graph with each of those (re-call this method??)
@@ -126,23 +143,7 @@ class ProteinLigandDataset(dgl.data.DGLDataset):
         return complex_graph
 
     def __len__(self):
-        return self.pharm_files.shape[0] - 1
-
-    def process(self):
-        # load data into memory
-        if not self.load_data:
-            self.pharm_segments = torch.tensor([0])
-        else:
-
-            with open(self.data_file, 'rb') as f:
-                data = pickle.load(f)
-            
-            self.pharm_pos = data['pharm_pos']
-            self.pharm_feat = data['pharm_feat']
-            self.prot_pos = data['prot_pos']
-            self.prot_feat = data['prot_feat']
-            self.prot_files = data['prot_file_name'] ## filepath to receptor file (only for val/test splits)
-            self.pharm_files = data['pharm_file_name'] ## filepath to ligand file (only for val/test splits)
+        return self.prot_file_name.shape[0] - 1
 
     def lig_atom_idx_to_element(self, element_idxs: List[int]):
         atom_elements = [ self.lig_reverse_map[element_idx] for element_idx in element_idxs ]
@@ -177,6 +178,54 @@ def one_hot_encode_prots(arr, ele_idx_map, num_ele):
         one_hot[i, ele_idx_map[e]] = 1
     return one_hot
 
+def build_initial_complex_graph(rec_atom_positions: torch.Tensor, rec_atom_features: torch.Tensor, pocket_res_idx: torch.Tensor, n_keypoints: int, cutoffs: dict, lig_atom_positions: torch.Tensor = None, lig_atom_features: torch.Tensor = None):
+
+    if (lig_atom_positions is not None) ^ (lig_atom_features is not None):
+        raise ValueError('ligand position and features must be either be both supplied or both left as None')
+
+    n_rec_atoms = rec_atom_positions.shape[0]
+
+    if lig_atom_positions is None:
+        n_lig_atoms = 0
+    else:
+        n_lig_atoms = lig_atom_positions.shape[0]
+    
+
+    # i've initialized this as an empty dict just to make clear the different types of edges in graph and their names
+    no_edges = ([], [])
+    graph_data = {
+        ('prot', 'pp', 'prot'): no_edges,
+        ('prot', 'pf', 'pharm'): no_edges,
+        ('pharm', 'ff', 'pharm'): no_edges,
+        ('pharm', 'fp', 'prot'): no_edges
+    }
+
+    # compute rec atom -> rec atom edges
+    pp_edges = radius_graph(rec_atom_positions, r=cutoffs['pp'], max_num_neighbors=100)
+    graph_data[('prot', 'pp', 'prot')] = (pp_edges[0], pp_edges[1])
+
+    # compute "same residue" feature ofr every rr edge
+    same_res_edge = pocket_res_idx[pp_edges[0]] == pocket_res_idx[pp_edges[1]]
+
+    num_nodes_dict = {
+        'rec': n_rec_atoms,'lig': n_lig_atoms
+    }
+
+    # create graph object
+    g = dgl.heterograph(graph_data, num_nodes_dict=num_nodes_dict)
+
+    # add node data
+    if lig_atom_positions is not None:
+        g.nodes['lig'].data['x_0'] = lig_atom_positions
+        g.nodes['lig'].data['h_0'] = lig_atom_features
+    g.nodes['rec'].data['x_0'] = rec_atom_positions
+    g.nodes['rec'].data['h_0'] = rec_atom_features
+    
+    # add edge data
+    g.edges['pp'].data['same_res'] = same_res_edge.view(-1, 1)
+
+    return g
+
 def collate_fn(examples: list):
 
     # break receptor graphs, ligand positions, and ligand features into separate lists
@@ -186,7 +235,7 @@ def collate_fn(examples: list):
     complex_graphs = dgl.batch(complex_graphs)
     return complex_graphs, interface_points
 
-def get_dataloader(dataset: ProteinLigandDataset, batch_size: int, num_workers: int = 1, **kwargs) -> GraphDataLoader:
+def get_dataloader(dataset: ProteinPharmacophoreDataset, batch_size: int, num_workers: int = 1, **kwargs) -> GraphDataLoader:
 
     dataloader = GraphDataLoader(dataset, batch_size=batch_size, drop_last=False, num_workers=num_workers, collate_fn=collate_fn, **kwargs)
     return dataloader
