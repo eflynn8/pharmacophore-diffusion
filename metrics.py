c3d99cd0c0a9e0fed91f71dc8fa0f8b93683188a
Riya <ras425@pitt.edu>
2024-04-23 10:07:11 -0400

    edit function

--- metrics.py
+++ metrics.py
@@ -1,35 +1,37 @@
 import torch
 from constants import ph_idx_to_type
 
-
-def compute_complementarity(pharm_feat, pharm_pos, prot_ph_feat, prot_ph_pos, cutoff=8.0):
-    #different cutoffs for diff interactions see rishal code
+def compute_complementarity(pharm_feat, pharm_pos, prot_ph_feat, prot_ph_pos):
     #returns fraction of pharmacophore features that are close to a complementary feature in the binding pocket
-    
-    compatible_types = {
+
+    matching_types = {
         'Aromatic': ['Aromatic', 'PositiveIon'],
         'HydrogenDonor': ['HydrogenAcceptor'],
-        'HydrogenAcceptor': ['HydrogenDonor', 'Aromatic'],
+        'HydrogenAcceptor': ['HydrogenDonor'],
         'PositiveIon': ['NegativeIon', 'Aromatic'],
         'NegativeIon': ['PositiveIon'],
         'Hydrophobic': ['Hydrophobic']
     }
-    # distances between pharmacophore and receptor features
-    distances = torch.cdist(pharm_pos, prot_ph_pos)
-    # mask where distance is within the cutoff
-    distance_mask = distances <= cutoff
+    matching_distance = {"Aromatic" : 7, 
+        "Hydrophobic": 5,
+        "HydrogenAcceptor": 4,
+        "HydrogenDonor": 4,
+        "NegativeIon": 5,
+        "PositiveIon": 5 }
     
-    # get feature types from one-hot encoding
     pharm_types = [ph_idx_to_type[idx] for idx in pharm_feat.argmax(dim=1)]
     prot_ph_types = [ph_idx_to_type[idx] for idx in prot_ph_feat.argmax(dim=1)]
-    
-    # mask of compatible receptor features for each pharmacophore feat
-    compatible = torch.tensor([[rec_type in compatible_types[ph_type] for rec_type in prot_ph_types]
+
+    # distances between pharmacophore and receptor features
+    distances = torch.cdist(pharm_pos, prot_ph_pos)
+    ph_matching_distances = torch.tensor([matching_distance[ph_type] for ph_type in pharm_types]).reshape(-1, 1)
+
+    # mask of complementary receptor features for each pharmacophore feature
+    matching = torch.tensor([[rec_type in matching_types[ph_type] for rec_type in prot_ph_types]
                                 for ph_type in pharm_types])
-    # combine distance and compatibility masks
-    mask = distance_mask & compatible
-    # count the number of ph features w/ rec complements within cutoff
-    compatible_count = mask.any(dim=1).sum()
+
+    mask = (distances <= ph_matching_distances) & matching
+    complement_count = mask.any(dim=1).sum()
     
-    fraction = compatible_count / len(pharm_feat)
+    fraction = complement_count / len(pharm_feat)
     return fraction
