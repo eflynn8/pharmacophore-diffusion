0afb5931b52b635cf68f91e26fef3cde5057b0b4
RishalAggarwal <rishal.aggarwal@gmail.com>
2024-10-24 13:38:11 -0400

    endpoint fixes and gvp cross attention

--- pharmacoforge/models/dynamics_gvp.py
+++ pharmacoforge/models/dynamics_gvp.py
@@ -10,7 +10,7 @@ from pharmacoforge.utils.graph_ops import add_pharm_edges, remove_pharm_edges
 
 class NoisePredictionBlock(nn.Module):
 
-    def __init__(self, in_scalar_dim: int, out_scalar_dim: int, vector_size: int, n_gvps: int = 3, intermediate_scalar_dim: int = 64):
+    def __init__(self, in_scalar_dim: int, out_scalar_dim: int, vector_size: int, n_gvps: int = 3, intermediate_scalar_dim: int = 64, gvp_cross_attention: bool = False):
         super().__init__()
 
         self.gvps = []
@@ -29,7 +29,8 @@ class NoisePredictionBlock(nn.Module):
                 dim_vectors_out=dim_vectors_out,
                 dim_feats_in=in_scalar_dim,
                 dim_feats_out=dim_feats_out,
-                vectors_activation=vectors_activation
+                vectors_activation=vectors_activation,
+                gvp_cross_attention=gvp_cross_attention
             ))
         self.gvps = nn.Sequential(*self.gvps)
 
--- pharmacoforge/models/fm_vector_field.py
+++ pharmacoforge/models/fm_vector_field.py
@@ -89,6 +89,7 @@ class FMVectorField(nn.Module):
 
         # fish cp_dim out of conv_config
         cp_dim = conv_config['cp_dim']
+        gvp_cross_attention=conv_config['gvp_cross_attention']
 
         # create molecule update layers
         self.node_position_updaters = nn.ModuleList([])
@@ -97,7 +98,7 @@ class FMVectorField(nn.Module):
         else:
             n_updaters = 1
         for _ in range(n_updaters):
-            self.node_position_updaters.append(NodePositionUpdate(node_scalar_dim, node_vector_dim, n_gvps=3, n_cp_feats=cp_dim))
+            self.node_position_updaters.append(NodePositionUpdate(node_scalar_dim, node_vector_dim, n_gvps=3, n_cp_feats=cp_dim,gvp_cross_attention=gvp_cross_attention))
 
 
         # create embedding layers for pharmacophore and protein atom types
@@ -485,7 +486,7 @@ class FMVectorField(nn.Module):
 
 class NodePositionUpdate(nn.Module):
 
-    def __init__(self, n_scalars, n_vec_channels, n_gvps: int = 3, n_cp_feats: int = 0):
+    def __init__(self, n_scalars, n_vec_channels, n_gvps: int = 3, n_cp_feats: int = 0, gvp_cross_attention: bool = False):
         super().__init__()
 
         self.gvps = []
@@ -507,6 +508,7 @@ class NodePositionUpdate(nn.Module):
                     n_cp_feats=n_cp_feats,
                     vectors_activation=vectors_activation,
                     vector_gating=True,
+                    cross_attention=gvp_cross_attention
                 )
             )
         self.gvps = nn.Sequential(*self.gvps)
